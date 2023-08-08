86cbcb91c84045cde235ce564d8a3cce08f853dd
unknown <thearnas4@gmail.com>
2023-08-08 21:47:01 +0300

    [Add] Adding Framework functions to be easily customizable

--- server/functions.lua
+++ server/functions.lua
@@ -1,1 +1,17 @@
 ESX = exports['es_extended']:getSharedObject()
+
+_Notification = function(source, msg)
+    TriggerClientEvent('esx:showNotification', source, msg)
+end
+
+_GetPlayerFromId = function(source)
+    return ESX.GetPlayerFromId(source)
+end
+
+_GetPlayerIdentifier = function(source)
+    return ESX.GetPlayerFromId(source).getIdentifier()
+end
+
+_GetPlayerName = function(source)
+    return ESX.GetPlayerFromId(source).getName()
+end
