163259b6b85ca3bef8c91ef4e48b36093c88f74d
unknown <thearnas4@gmail.com>
2023-08-08 21:45:06 +0300

    [Add] New Locales for notifications

--- locales/en.lua
+++ locales/en.lua
@@ -1,10 +1,21 @@
 Locales['en'] = {
-    ['enter'] = 'Press ~INPUT_CONTEXT~ To Open~b~ Apartament Actions',
+    ['menu_enter_title'] = 'Mazebank Garage',
+    ['menu_enter_purchase'] = 'Purchase Garage',
+    ['menu_enter_garage'] = 'Enter Garage',
+    ['menu_enter_visit'] = 'Visit Garage',
+    ['menu_enter_visit_desc'] = 'Visit other Player\'s Grarages',
+    ['menu_visit_title'] = 'Input Garage\'s ID',
+    ['price'] = 'Price: %s',
+    ['id'] = 'ID',
+    ['enter'] = 'Enter',
+
+    ['notification_invite_requested'] = 'You\'ve Requested To Enter %s\ Garage',
+    ['notification_invite_request'] = 'New Request to come inside, incoming'
+
+    -- ['enter'] = 'Press ~INPUT_CONTEXT~ To Open~b~ Apartament Actions',
     ['exit'] = 'Press ~INPUT_CONTEXT~ To~r~ Exit Apartament',
     ['enter_garage'] = 'Enter Garage',
-    ['visit_garage'] = 'Visit',
     ['buy_garage'] = 'Buy Garage: $%s',
-    ['menu_enter_title'] = 'Mazebank Garage',
     ['notification_buy_okay'] = 'You successfully bought~b~ MazeBank Garage',
     ['notification_buy_not'] = 'You don\'t have enough money!'
 }
