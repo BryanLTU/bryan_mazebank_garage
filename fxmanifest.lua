6265681d98377516db0975c2a91e813f2e5e4e47
unknown <thearnas4@gmail.com>
2023-08-08 20:53:42 +0300

    [Update] Added Ox Lib init

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,10 +3,15 @@ game 'gta5'
 
 author 'BryaN'
 
-shared_script 'config.lua'
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
 client_scripts {
     'client/*.lua'
 }
+
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/*.lua'
