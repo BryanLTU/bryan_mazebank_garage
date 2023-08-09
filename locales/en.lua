efa7948b4756e86f8dca1503bef4efdc2c497d04
unknown <thearnas4@gmail.com>
2023-08-09 21:22:37 +0300

    [Add] Added new Strings

--- locales/en.lua
+++ locales/en.lua
@@ -1,16 +1,23 @@
 Locales['en'] = {
-    ['menu_enter_title'] = 'Mazebank Garage',
+    ['menu_title'] = 'Mazebank Garage',
     ['menu_enter_purchase'] = 'Purchase Garage',
     ['menu_enter_garage'] = 'Enter Garage',
     ['menu_enter_visit'] = 'Visit Garage',
     ['menu_enter_visit_desc'] = 'Visit other Player\'s Grarages',
     ['menu_visit_title'] = 'Input Garage\'s ID',
-    ['price'] = 'Price: %s',
+    ['menu_enter_vehicle'] = 'Park your vehicle in the Garage',
+    ['price'] = 'Price: <span style="color:lime;">$%s</span>',
     ['id'] = 'ID',
     ['enter'] = 'Enter',
 
     ['notification_invite_requested'] = 'You\'ve Requested To Enter %s\ Garage',
-    ['notification_invite_request'] = 'New Request to come inside, incoming'
+    ['notification_invite_instance_does_not_exist'] = 'Garage with such ID is not Available',
+    ['notification_invite_request'] = 'New Request to come inside, incoming',
+    ['notification_enter_garage_not_owned'] = 'You don\'t Own this Garage',
+    ['notification_vehicle_not_owned'] = 'You don\'t Own this Vehicle',
+    ['notification_buy_not_enough_money'] = 'You don\'t have enough Money',
+    ['notification_enter_too_far_away'] = 'Your position is too far away from the Entrance',
+    ['notification_garage_full'] = 'Your Garage is fully Occupied with vehicles',
 
     -- ['enter'] = 'Press ~INPUT_CONTEXT~ To Open~b~ Apartament Actions',
     ['exit'] = 'Press ~INPUT_CONTEXT~ To~r~ Exit Apartament',
