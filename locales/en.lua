4bf067ca286d8b62c5e2460629864cfa7565cca9
unknown <thearnas4@gmail.com>
2023-08-11 23:18:11 +0300

    [Add] Added new locales

--- locales/en.lua
+++ locales/en.lua
@@ -6,6 +6,8 @@ Locales['en'] = {
     ['menu_enter_visit_desc'] = 'Visit other Player\'s Grarages',
     ['menu_visit_title'] = 'Input Garage\'s ID',
     ['menu_enter_vehicle'] = 'Park your vehicle in the Garage',
+    ['menu_title_id'] = 'Mazebank Garage [%s]',
+
     ['price'] = 'Price: $%s',
     ['count'] = 'Count: %s',
     ['id'] = 'ID',
@@ -23,7 +25,7 @@ Locales['en'] = {
     ['warning'] = 'Warning!',
     ['exit_with_visitors_warning'] = 'You\'re about to exit the Garage while there\'re still some Visitors. If you procceed all the Visitors will be Kicked Out. Do you procceed?',
 
-    ['notification_invite_requested'] = 'You\'ve Requested To Enter %s\ Garage',
+    ['notification_invite_requested'] = 'You\'ve Requested To Enter %s Garage',
     ['notification_invite_instance_does_not_exist'] = 'Garage with such ID is not Available',
     ['notification_invite_request'] = 'New Request to come inside, incoming',
     ['notification_enter_garage_not_owned'] = 'You don\'t Own this Garage',
@@ -37,12 +39,17 @@ Locales['en'] = {
     ['alert_vehicle_managment_position'] = '~INPUT_CELLPHONE_LEFT~ Previous / ~INPUT_CELLPHONE_RIGHT~ Next',
     ['alert_vehicle_managment_confirm'] = '~INPUT_CELLPHONE_SELECT~ Confirm',
     ['alert_vehicle_managment_cancel'] = '~INPUT_CELLPHONE_CANCEL~ Cancel',
+    ['alert_enter'] = '[E] - Enter Mazebank Garage',
+    ['alert_entervh'] = '[E] - Drive in Mazebank Garage',
+    ['alert_exit'] = '[E] - Manage Garage',
+    ['alert_unlock'] = 'Press [~b~E~s~] To ~g~Unlock~s~ Vehicle',
+    ['alert_lock'] = 'Press [~b~E~s~] To ~r~Lock~s~ Vehicle',
 
 
     -- ['enter'] = 'Press ~INPUT_CONTEXT~ To Open~b~ Apartament Actions',
-    ['exit'] = 'Press ~INPUT_CONTEXT~ To~r~ Exit Apartament',
-    ['enter_garage'] = 'Enter Garage',
-    ['buy_garage'] = 'Buy Garage: $%s',
-    ['notification_buy_okay'] = 'You successfully bought~b~ MazeBank Garage',
-    ['notification_buy_not'] = 'You don\'t have enough money!'
+    -- ['exit'] = 'Press ~INPUT_CONTEXT~ To~r~ Exit Apartament',
+    -- ['enter_garage'] = 'Enter Garage',
+    -- ['buy_garage'] = 'Buy Garage: $%s',
+    -- ['notification_buy_okay'] = 'You successfully bought~b~ MazeBank Garage',
+    -- ['notification_buy_not'] = 'You don\'t have enough money!'
 }
